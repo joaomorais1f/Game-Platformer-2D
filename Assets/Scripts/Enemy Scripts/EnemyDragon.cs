6c6b8d99e06528c0dd8d6ace5dc7b59022c9fda1
joaomorais1f <ifsp.joaov@gmail.com>
2019-10-18 22:07:42 -0300

    desenvolvimento do inimigo, dragão.

---
+++ Assets/Scripts/Enemy Scripts/EnemyDragon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDragon : MonoBehaviour {
+    public float speed, bound;
+    private bool up;
+    private Animator anim;
+    private Transform player;
+
+    void Awake() {
+        player = GameObject.Find("Assassin").transform;
+        anim = GetComponent<Animator>();    
+    }
+
+    void Start () {
+        bound = transform.position.y;
+    }
+
+    void Update () {
+        Movement();
+    }
+
+    void Movement () {
+        if (up) {
+            Vector3 pos = transform.position;
+            pos.y += speed;
+            transform.position = pos;
+            if (transform.position.y > bound + 0.13f) {
+                up = false;
+            }
+        } else {
+            Vector3 pos = transform.position;
+            pos.y -= speed;
+            transform.position = pos;
+            if (transform.position.y < bound - 0.13f) {
+                up = true;
+            }
+        }
+        if (transform.position.x < player.transform.x) {
+            transform.localScale = new Vector3(1,1,1);
+        }
+
+        if (transform.position.x > player.transform.x) {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+    }
+}
